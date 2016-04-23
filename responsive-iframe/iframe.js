1bc08741c3a783aa70173ad343ee56899d27e750
Ire Aderinokun <ire@ireaderinokun.com>
2016-04-23 11:32:16 +0100

    initial commit

---
+++ responsive-iframe/iframe.js
@@ -0,0 +1,24 @@
+// Get height of the main element in the iframe document
+var documentHeight = document.getElementsByClassName('main-element')[0].scrollHeight
+
+// Add some unique identifier to the string being passed
+var message = 'documentHeight:'+documentHeight;
+
+// Pass message to (any*) parent document
+parent.postMessage(message,"*");
+
+
+// On resize of the window, recalculate the height of the main element, and pass to the parent document again
+window.onresize = function(event) {
+	var newDocumentHeight = document.getElementsByClassName('main-element')[0].scrollHeight;
+	var heightDiff = documentHeight - newDocumentHeight;
+
+	// If difference between current height and new height is more than 10px
+	if ( heightDiff > 10 | heightDiff < -10 ) {
+
+		documentHeight = newDocumentHeight;
+		message = 'documentHeight:'+documentHeight;
+		parent.postMessage(message,"*");
+	}
+	
+} 
---
+++ responsive-iframe/parent.js
@@ -0,0 +1,18 @@
+// Add event listener for messages being massed from the iframe
+var eventMethod = window.addEventListener ? "addEventListener" : "attachEvent";
+var eventer = window[eventMethod];
+var messageEvent = eventMethod == "attachEvent" ? "onmessage" : "message";
+
+eventer(messageEvent,function(e) {
+	
+	// Check that message being passed is the documentHeight
+	if (  (typeof e.data === 'string') && (e.data.indexOf('documentHeight') > -1) ) {
+
+	 	var height = e.data.split('documentHeight:')[1],
+	 		height = parseInt(height) + 50; // add a bit of extra space
+
+	 	// Change height of iframe
+	 	document.getElementById('myIframe').height = height + 'px';
+
+	} 
+},false);
