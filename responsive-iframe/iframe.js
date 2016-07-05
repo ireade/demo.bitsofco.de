bb6e5f5d6ccbb018f56ba472132c33767a859064
Ire Aderinokun <ire@ireaderinokun.com>
2016-07-05 09:06:38 +0100

    the target trick

--- responsive-iframe/iframe.js
+++ responsive-iframe/iframe.js
@@ -1,15 +1,15 @@
 // Get height of the main element in the iframe document
-var documentHeight = document.getElementsByClassName('main-element')[0].scrollHeight
+var documentHeight = document.getElementsByClassName('main-element')[0].scrollHeight;
 
 // Add some unique identifier to the string being passed
 var message = 'documentHeight:'+documentHeight;
 
 // Pass message to (any*) parent document
-parent.postMessage(message,"*");
+parent.postMessage(message,'*');
 
 
 // On resize of the window, recalculate the height of the main element, and pass to the parent document again
-window.onresize = function(event) {
+window.onresize = function() {
 	var newDocumentHeight = document.getElementsByClassName('main-element')[0].scrollHeight;
 	var heightDiff = documentHeight - newDocumentHeight;
 
@@ -18,7 +18,9 @@ window.onresize = function(event) {
 
 		documentHeight = newDocumentHeight;
 		message = 'documentHeight:'+documentHeight;
-		parent.postMessage(message,"*");
+		parent.postMessage(message,'*');
 	}
 	
-} 
+} ;
+
+
